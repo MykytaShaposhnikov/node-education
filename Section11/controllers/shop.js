62903aac6f705742d22166581adbc6daa5ae492e
MykytaShaposhnikov <mykyta.shaposhnikov@encirclelabs.com>
2022-08-29 13:02:20 +0300

    Started using sequelize

--- Section11/controllers/shop.js
+++ Section11/controllers/shop.js
@@ -2,20 +2,22 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-      .then(([rows,fieldData])=>res.render('shop/product-list', {
-        prods: rows,
-        pageTitle: 'All Products',
-        path: '/products'
-      }))
-      .catch(err=>console.log(err))
+    Product.findAll()
+        .then((prods)=>
+            res.render('shop/product-list', {
+            prods,
+            pageTitle: 'All Products',
+            path: '/products'
+        }))
+        .catch(err=>console.log(err))
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-      .then(([product,fieldData])=>  res.render('shop/product-detail', {
-          product: product[0],
+  Product.findByPk(prodId)
+      .then((product)=>
+          res.render('shop/product-detail', {
+          product: product,
           pageTitle: product.title,
           path: '/products'
       }))
@@ -23,13 +25,13 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-      .then(([rows,fieldData])=> res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
-      }))
-      .catch(err=>console.log(err))
+    Product.findAll()
+        .then((prods)=> res.render('shop/index', {
+            prods,
+            pageTitle: 'Shop',
+            path: '/'
+        }))
+        .catch(err=>console.log(err))
 };
 
 exports.getCart = (req, res, next) => {
--- Section11/util/database.js
+++ Section11/util/database.js
@@ -1,10 +1,8 @@
-const mysql=require('mysql2');
+const Sequelize=require('sequelize')
 
-const pool=mysql.createPool({
+const sequelize= new Sequelize('node-complete','root','NikShapa99',{
     host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'NikShapa99',
-})
+    dialect:'mysql'
+});
 
-module.exports=pool.promise()
+module.exports=sequelize;
